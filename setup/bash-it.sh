b10ef6cd3e3b357bcf52a46e8a28604f17e1be0e
Steve Ellis <email@steveell.is>
2018-04-14 12:07:35 -0700

    add pivotal bash-it theme

--- setup/bash-it.sh
+++ setup/bash-it.sh
@@ -1,5 +1,6 @@
 rm -rf ~/.bash_it
 git clone https://github.com/Bash-it/bash-it.git ~/.bash_it
+cp config/bobby_pivotal.theme ~/.bash_it/themes/bobby/bobby.theme.bash
 source ~/.bash_profile
 bash-it enable completion git
 bash-it enable plugin ssh
