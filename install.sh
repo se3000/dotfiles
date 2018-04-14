7bb1210cf0db349bbfa90167d9cc6c6138e222a7
Steve Ellis <email@steveell.is>
2018-04-14 11:21:46 -0700

    consolidate and rename files

--- install.sh
+++ install.sh
@@ -11,13 +11,16 @@ for file in "$DOTFILES_DIR/macos/*"; do
   [[ -r $file ]] && . $file;
 done
 
-. "$DOTFILES_DIR/install/brew.sh"
-. "$DOTFILES_DIR/install/git.sh"
-. "$DOTFILES_DIR/install/npm.sh"
-. "$DOTFILES_DIR/install/bash.sh"
-. "$DOTFILES_DIR/install/bash-it.sh"
-. "$DOTFILES_DIR/install/brew-cask.sh"
-. "$DOTFILES_DIR/install/tmux.sh"
-. "$DOTFILES_DIR/install/vim.sh"
+. "$DOTFILES_DIR/setup/brew.sh"
+. "$DOTFILES_DIR/setup/git.sh"
+. "$DOTFILES_DIR/setup/npm.sh"
+. "$DOTFILES_DIR/setup/bash.sh"
+. "$DOTFILES_DIR/setup/bash-it.sh"
+. "$DOTFILES_DIR/setup/brew-cask.sh"
+. "$DOTFILES_DIR/setup/tmux.sh"
+. "$DOTFILES_DIR/setup/vim.sh"
+. "$DOTFILES_DIR/defaults-chrome.sh"
+. "$DOTFILES_DIR/defaults.sh"
+. "$DOTFILES_DIR/dock.sh"
 
 echo "FIN!"
