2f9f6dcd4f40771ee403f9114e15a8aa897f7db3
Steve Ellis <email@steveell.is>
2021-12-01 23:42:29 -0500

    cleanup homebrew installs

--- setup/brew.sh
+++ setup/brew.sh
@@ -6,52 +6,29 @@ brew upgrade
 # Install packages
 
 apps=(
-  ag
-  ack
   asdf
   bash-completion2
-  bat
   circleci
   coreutils
-  ctags
-  dep
   direnv
-  dockutil
   docker-compose
-  ethereum/ethereum/solidity
-  fswatch
   fzf
   git
-  git-flow
-  git-extras
   github/gh/gh
   go
   grc
   grep
-  hexyl
   jq
-  kubectl
-  lynx
-  m-cli
-  md5sha1sum
-  npm
   postgres
   python
   ripgrep
   tmux
-  travis
-  watch
   vim
-  wget
-  wireguard-tools
   yarn
   ykman
-  youtube-dl
 )
 
 brew install "${apps[@]}"
 
 . /usr/local/opt/fzf/install
 source ~/.bash_profile
-nvm install 10.16.3
-nvm alias default node
