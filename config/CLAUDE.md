f8d7b973da025d8ae69bfa34a40e2eb0fd751b80
Steve Ellis <email@steveell.is>
2026-06-29 10:59:48 -0400

    add claude.md to shared config files

---
+++ config/CLAUDE.md
@@ -0,0 +1,1 @@
+Always use git worktrees.
--- symlink_config_files.sh
+++ symlink_config_files.sh
@@ -3,9 +3,12 @@
 export DOTFILES_DIR
 DOTFILES_DIR="$( cd "$( dirname "${ZSH_SOURCE[0]}" )" && pwd )"
 
-echo "symlinking dotfiles..."
+echo "symlinking config files..."
 
+# symlink dot files
 for file in "$DOTFILES_DIR/config/.*"; do
   [[ $file == *.example ]] && continue
   ln -sfv $file ~
 done
+
+ln -sfv "$DOTFILES_DIR/config/CLAUDE.md" ~
